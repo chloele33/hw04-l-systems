7e3fcd2e3fc279e1f02b9cd5bb343bb37f381fc2
chloele33 <chloele@seas.upenn.edu>
2019-02-27 22:36:59 -0500

    modified expansion rules

--- src/ExpansionRule.ts
+++ src/ExpansionRule.ts
@@ -1,9 +1,9 @@
-class ExpansionRule {
+export default class ExpansionRule {
 
-    expMap: Map<string, Set<Map<string, number>>>; // string mapping to a set of string and probability pairs
+    expMap: Map<string, Array<[string, number]>>; // string mapping to a set of string and probability pairs
 
     constructor() {
-        this.expMap = new Map<string, Set<Map<string, number>>>();
+        this.expMap = new Map<string, Array<[string, number]>>();
     }
 
     expand(str: string) : string {
@@ -12,14 +12,12 @@ class ExpansionRule {
         } else {
             var xi: number = Math.random();
             var result : string;
-            var ruleSet = this.expMap.get(str);
+            var ruleArray = this.expMap.get(str);
             var sumProb = 0.0;
             // iterate through the set
-            for (var it = ruleSet.values(), currPair= null; currPair=it.next().value; ) {
-                var stringKey = currPair.keys().next().value;
-                var p = currPair.get(stringKey);
+            for (const [s, p] of ruleArray) {
                 if (xi > sumProb && xi <= sumProb + p) {
-                    result = stringKey;
+                    result = s;
                 }
                 sumProb += p;
             }
@@ -31,16 +29,12 @@ class ExpansionRule {
     // adds to the expansion rule, mapping from the original to the new string with a probability
     set(oriStr: string, newStr: string, prob: number) {
         if(this.expMap.has(oriStr)){
-            this.expMap.get(oriStr).add(new Map<string, number>().set(newStr, prob)); // override previous rule
+            this.expMap.get(oriStr).push([newStr, prob]); // override previous rule
         }
         else{
-            var newRule = new Map<string, number>().set(newStr, prob);
-            var newSet = new Set<Map<string, number>>();
-            newSet.add(newRule);
-            this.expMap.set(oriStr, newSet);
+            var newRule : Array<[string, number]> = [[newStr, prob]];
+            this.expMap.set(oriStr, newRule);
         }
 
     }
 }
-
-export default ExpansionRule;
